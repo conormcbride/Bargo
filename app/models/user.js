30cb11999aa00211ae9be8ca0cf263377c8256f1
Conor McBride <conormcbride158@gmail.com>
2018-03-08 17:51:01 +0000

    Pushing up incase theme breaks app

--- app/models/user.js
+++ app/models/user.js
@@ -7,6 +7,7 @@ var UserSchema = new Schema({
 
     username: { type: String, lowercase: true, required: true, unique: true },
     password: { type: String, required: true},
+    // role:{type: String, required: true },
     email: { type: String, required: true, lowercase: true, unique: true}
     // img: { data: Buffer, contentType: String }
 
