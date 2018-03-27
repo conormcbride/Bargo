3e5a3c216eac16118a37c31d161cd12ac4749334
Conor McBride <conormcbride158@gmail.com>
2018-03-27 21:01:31 +0100

    Validation finished

--- app/models/user.js
+++ app/models/user.js
@@ -1,35 +1,89 @@
-var mongoose = require('mongoose'); // Import Mongoose Package
-var Schema = mongoose.Schema; // Assign Mongoose Schema function to variable
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
+var titlize = require('mongoose-title-case');
+var validate = require('mongoose-validator')
 
-// User Mongoose Schema
+
+var nameValidator = [
+    validate({
+        validator: 'matches',
+        arguments: /^(([a-zA-Z]{3,20})+[ ]+([a-zA-Z]{3,20})+)+$/,
+        message:'Name Must be at least 3 characters and no more than 30, no special characters, no numbers, must have space between name'
+    }),
+    validate({
+        validator: 'isLength',
+        arguments: [3, 25],
+        message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
+    })
+];
+
+var emailValidator = [
+    validate({
+        validator: 'isEmail',
+
+        message:'not valid email '
+    }),
+    validate({
+        validator: 'isLength',
+        arguments: [3, 25],
+        message: 'email should be between {ARGS[0]} and {ARGS[1]} characters'
+    })
+];
+
+var usernameValidator = [
+    validate({
+        validator: 'isLength',
+        arguments: [3, 25],
+        message: 'Username should be between {ARGS[0]} and {ARGS[1]} characters'
+    }),
+    validate({
+        validator:'isAlphanumeric',
+        message:'Username must contain numbers and letters only'
+    })
+];
+
+
+
+var passwordValidator = [
+    validate({
+        validator: 'matches',
+        arguments: /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{5,35}$/,
+        message:'Password Must be at least 5 characters and no more than 35, must contain at lease 1 lowercase, 1 uppercase, 1 special character'
+    }),
+    validate({
+        validator: 'isLength',
+        arguments: [5, 35],
+        message: 'Password should be between {ARGS[0]} and {ARGS[1]} characters'
+    })
+];
+
+
+
 var UserSchema = new Schema({
+    name:{ type: String, required: true,validate: nameValidator},
+    username: { type: String, lowercase: true, required: true, unique: true, validate:usernameValidator },
+    password: { type: String, required: true, validate: passwordValidator},
+    email: { type: String, required: true, lowercase: true, unique: true, validate: emailValidator}
 
-    username: { type: String, lowercase: true, required: true, unique: true },
-    password: { type: String, required: true},
-    // role:{type: String, required: true },
-    email: { type: String, required: true, lowercase: true, unique: true}
-    // img: { data: Buffer, contentType: String }
 
 });
-
-// Middleware to ensure password is encrypted before saving user to database
 UserSchema.pre('save', function(next) {
-    var user = this; // apply to current user running through middleware
-
-    // if (!user.isModified('password')) return next(); // If password was not changed or is new, ignore middleware
-
-    // Function to encrypt password
+    var user = this;
     bcrypt.hash(user.password, null, null, function(err, hash) {
-        if (err) return next(err); // Exit if error is found
-        user.password = hash; // Assign the hash to the user's password so it is saved in database encrypted
-        next(); // Exit Bcrypt function
+        if (err) return next(err);
+        user.password = hash;
+        next();
     });
 });
+UserSchema.plugin(titlize, {
+    paths: [ 'name' ]
+});
 
-//custom method for database
 UserSchema.methods.comparePassword = function (password) {
     return bcrypt.compareSync(password, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema); // Export User Model for us in API
+module.exports = mongoose.model('User', UserSchema);
+
+
--- app/routes/api.js
+++ app/routes/api.js
@@ -14,22 +14,45 @@ module.exports =  function(router){
     user.username = req.body.username
     user.password = req.body.password
     user.email = req.body.email
-
-    if (req.body.username == null || req.body.username =='' || req.body.password == null || req.body.password =='' || req.body.email == null || req.body.email ==''){
+    user.name = req.body.name;
+    if (req.body.username == null || req.body.username =='' || req.body.password == null ||
+        req.body.password =='' || req.body.email == null || req.body.email ==''|| req.body.name == null || req.body.name ==''){
         res.json({
             success:false,
             message:'Ensure nothing is left empty'
         })
     }else user.save(function (err) {
         if( err){
+
+
+
+           if(err.errors != null){
+               if (err.errors.name) {
+                   res.json({message: err.errors.name.message});
+               }else if(err.errors.email){
+                   res.json({message: err.errors.email.message});
+
+               }else if(err.errors.username){
+                   res.json({message: err.errors.username.message});
+
+               }else if(err.errors.password){
+                   res.json({message: err.errors.password.message});
+
+               } else{
+                   res.json({success:false, message: err})
+               }
+
+           }else if (err){
+              if(err.code == 11000){
+                  res.json({ success:false, message:'Username or Email already taken'})
+              }else{
+                  res.json({success:false, message: err})
+              }
+           }
+
+        }  else{
             res.json({
-                success:false,
-                message:'Username or Email already exists'
-            })
-        }else{
-            res.json({
-                success:true,
-                message:'User Created!'})
+                success:true, message:'User Created!'})
     }
     });
 
